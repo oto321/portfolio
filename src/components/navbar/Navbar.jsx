b6dbbc30d2be0bb80e1926eeb72b8c0ff58cdc8f
oto <navrajmann138@gmail.com>
2025-07-08 15:02:22 +1200

    changed the sidebar and the background

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,40 +1,76 @@
+
+function HomeIcon() {
+  return (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+  </svg>
+  );
+}
+
+function ContactIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H6.911a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661Z" />
+    </svg>
+
+  );
+}
+
+function AboutIcon() {
+  return (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
+  </svg>
+  );
+}
+
+function ProjectsIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 0 0-1.883 2.542l.857 6a2.25 2.25 0 0 0 2.227 1.932H19.05a2.25 2.25 0 0 0 2.227-1.932l.857-6a2.25 2.25 0 0 0-1.883-2.542m-16.5 0V6A2.25 2.25 0 0 1 6 3.75h3.879a1.5 1.5 0 0 1 1.06.44l2.122 2.12a1.5 1.5 0 0 0 1.06.44H18A2.25 2.25 0 0 1 20.25 9v.776" />
+    </svg>
+  );
+}
+
+
+
 function NavBar() {
   return (
-    <div>
-      <nav className="rounded-2xl bg-cyan-950 py-2 px-9 w-fit">
+    <div className="flex justify-center ">
+      <nav className="
+    rounded-2xl bg-neutral-800 backdrop-blur-3xl w-fit px-5 py-3
+    border-b-2 border-b-neutral-600 border-t-2 border-t-neutral-300
+    hover:drop-shadow-lg hover:drop-shadow-cyan-400/50 hover:px-10
+    transition-all duration-300 linear
+    ">
         <ul className="inline-flex gap-5">
-          <Item text={"Index"} />
-          <Item text={"About"} />
-          <Item text={"Projects"} />
-          <Item text={"Contact"} />
+          <Item image={HomeIcon} />
+          <Item image={AboutIcon} />
+          <Item image={ProjectsIcon} />
+          <Item image={ContactIcon} />
         </ul>
       </nav>
     </div>
   );
 }
 
-function Item({ text }) {
+function Item({ image }) {
+  const ImageComponent = image;
   return (
     <li style={{ fontFamily: "Orbitron" }}>
       <button
-        className=" 
-    relative text-lg text-white py-2 px-4 overflow-hidden group transition-colors duration-200 ease-in-out
-    hover:text-cyan-400  rounded-full
-          ">
-        <span
+        className="
+          relative text-lg text-white font-extrabold py-2 px-4 overflow-hidden rounded-full 
+          hover:text-black group
+          transition-all duration-100 ease-in-out
+        "
+      >
+        <div
           className="
-    absolute  rounded-full top-0 inset-x-0 h-1 bg-cyan-600
-    transform scale-x-0 group-hover:scale-x-100
-    transition-transform duration-200 ease-out
-    "/>
-        {text}
-        <span
-          className="
-    absolute  rounded-full bottom-0 inset-x-0 h-1 bg-cyan-600
-    transform scale-x-0 group-hover:scale-x-100
-    transition-transform duration-200 ease-out
-    "
+            absolute inset-0 rounded-full bg-cyan-300
+            scale-x-0 group-hover:scale-x-100 -z-10
+            transition-all duration-300 ease-in-out
+          "
         />
+        <ImageComponent />
       </button>
     </li>
   );
